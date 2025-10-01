6542034419135a25e54ed371761aefbf1a414ca4
Jeanne Delattre <jdelattr@f6r10s15.clusters.42paris.fr>
2025-10-01 19:59:34 +0200

    01.09 19h59

--- linked_list.c
+++ linked_list.c
@@ -6,7 +6,7 @@
 /*   By: jdelattr <jdelattr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:19:29 by jdelattr          #+#    #+#             */
-/*   Updated: 2025/10/01 13:42:05 by jdelattr         ###   ########.fr       */
+/*   Updated: 2025/10/01 19:12:06 by jdelattr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,100 +51,156 @@
 // 	return (in_quote);
 // }
 
-int lexing_quotes(char *str, t_list **tok)
-{
-	char c;
-	char *p;
-	int i;
+// int lexing_quotes(char *str, t_list **tok, int *count)
+// {
+// 	char c;
+// 	char *p;
+// 	int i;
 	
-	c = str[0];
-	i = 1;
-	while(str[i] != c && str[i] != '\0')
-		i++;
-	if(str[i] == c)
-		p = strndup(str, i + 1);
-	else
-		p = strndup(str, i);
-	if(p == NULL)
-		return(ERROR);
-	if(fill_in_lst(tok, p) == ERROR)
-		return(ERROR);
-	return(0);
-}
-// a tester 
+// 	c = str[0];
+// 	i = 1;
+// 	while(str[i] != c && str[i] != '\0')
+// 		i++;
+// 	if(str[i] == c)
+// 		p = ft_strndup(str, i + 1);
+// 	else
+// 		p = ft_strndup(str, i);
+// 	if(p == NULL)
+// 		return(ERROR);
+// 	if(fill_in_lst(tok, p) == ERROR)
+// 		return(ERROR);
+// 	*count = ft_strlen(p);
+// 	return(0);
+// }
+// // a tester 
 
-int lexing_spaces(char *str, t_list **tok)
-{ 
-	char *sep;
-	int i;
-	char *p;
+
+// int lexing_word(char *str, t_list **tok, int *count)
+// { 
+// 	int i;
+// 	char *p;
 	
-	if(str == NULL)
-		return(0);
-	i = 0;
-	sep = " \t\n\v\f\r";
-	while(is_separator(str[i]) == IS && str[i] != '\0')
-		i++;
-	p = ft_strndup(str, i);
-	if(p == NULL)
-		return(ERROR);
-	if(fill_in_lst(tok, p) == ERROR)
-		return(ERROR);
-	return(0);
-}
-//plus ou moins goodjob
+// 	if(str == NULL)
+// 		return(0);
+
+// 	i = 0;
+// 	while(is_separator(str[i]) == IS_NOT && str[i] != '\0')
+// 		i++;
+// 	p = ft_strndup(str, i);
+// 	if(p == NULL)
+// 		return(ERROR);
+// 	if(fill_in_lst(tok, p) == ERROR)
+// 		return(free(p), ERROR);
+// 	*count = ft_strlen(p);
+// 	return(0);
+// }
 
 
-void lexing(char *str, t_list **tok)
-{
-	int i;
-	char *tik;
-	int value; 
-
-	i = 0;
-	while(str[i] != '\0')
-	{ 
-		if(is_separator(str[i]) == IS)
-			value = lexing_spaces(&str[i], tok);
-		else if(str[i] == "\"" || str[i] == "\'")
-			value = lexing_quotes(&str[i], tok);
-		else if(is_pipe_bracket(&str[i]) == IS)
-			value = lexing_like_pipe(&str[i], tok);
-		else if(str[i] == "$")
-			value = lexing_dollar(&str[i], tok);
-		else if(is_different(&str[i]) == IS)
-			value = lexing_word(&str[i], tok);
-		else if(str[i] == '\0')
-			break;
-		if(value == ERROR)
-			free_exit(*tok, GEN_ERRNO, NULL);
-		i++;
-	}
-}
+// int lexing_spaces(char *str, t_list **tok, int *count)
+// { 
+// 	char *sep;
+// 	int i;
+// 	char *p;
+	
+// 	if(str == NULL)
+// 		return(0);
+// 	i = 0;
+// 	sep = " \t\n\v\f\r";
+// 	while(is_spaces(str[i]) == IS && str[i] != '\0')
+// 		i++;
+// 	p = ft_strndup(str, i);
+// 	if(p == NULL)
+// 		return(ERROR);
+// 	if(fill_in_lst(tok, p) == ERROR)
+// 		return(free(p), ERROR);
+// 	*count = ft_strlen(p);
+// 	return(0);
+// }
+// //plus ou moins goodjob
 
 
-int	main(int ac, char **av)
-{
-	char * command_line;
-	t_list *tok;
-	tok = NULL;
-	while(1)
-	{ 
-		// command_line  = readline;
-		lexing(command_line, &tok);
 
-	}
-}
+// int	lexing_metachar(char *str, t_list **tok, int *count)
+// {
+// 	int	i;
+// 	char *p;
+	
+// 	if (str == NULL)
+// 		return (0);
+// 	i = 0;
+// 	if (str[0] == '<' && str[1] == '<')
+// 		p = strdup("<<");
+// 	else if (str[0] == '>' && str[1] == '>')
+// 		p = strdup(">>");
+// 	else if (str[0] == '<')
+// 		p = strdup("<");
+// 	else if (str[0] == '>')
+// 		p = strdup(">");
+// 	else if (str[0] == '$')
+// 		p = strdup("$");
+// 	else if (str[0] == '|')
+// 		p = strdup("|");
+// 	if(p == NULL)
+// 		return(ERROR);
+// 	*count = ft_strlen(p);
+// 	if(fill_in_lst(tok, p) == ERROR)
+// 		return(free(p), ERROR);
+// 	return(0);
+// }
+	
 
-/**
- * @brief 
- * @param lexing recupere la ligne de commande et creer des char *malloc
- * de tokens qui seront mis dans une t_list selon la categorie du token
- * @param lexing_spaces si le caractere str[i] est un espace on arrive dans
- * cette fonction. @param lexing_spaces malloc et remplit la liste avec un token
- * d'espaces.
- * @param lexing_quotes malloc + remplit les quotes
- * @param is_pipe_bracket malloc + remplit les pipe < > | 
- * @param lexing_dollar malloc + remplit les dollars (avec le nom de la variable)
- * @param lexing_word malloc + remplit un mot (tout caractere different des precedents)
- */
+
+// void lexing(char *str, t_list **tok)
+// {
+// 	int i;
+// 	char *tik;
+// 	int value; 
+
+// 	int size_tik;
+	
+
+// 	i = 0;
+// 	size_tik = 0;
+// 	while(str[i] != '\0')
+// 	{
+// 		if(is_spaces(str[i]) == IS)
+// 			value = lexing_spaces(&str[i], tok, &size_tik);
+// 		else if(is_separator (&str[i]) == IS_NOT)
+// 			value = lexing_word(&str[i], tok, &size_tik);
+// 		else if(is_quote(&str[i]) == IS)
+// 			value = lexing_quotes(&str[i], tok, &size_tik);
+// 		else if(is_metachar(&str[i], tok, &size_tik) == IS)
+// 			value = lexing_metachar(&str[i], tok, &size_tik);
+// 		if(value == ERROR)
+// 			free_exit(*tok, GEN_ERRNO, NULL);
+// 		i + size_tik;
+// 	}
+// }
+
+
+// int	main(int ac, char **av)
+// {
+// 	char * command_line;
+// 	t_list *tok;
+// 	tok = NULL;
+// 	while(1)
+// 	{ 
+// 		// command_line  = readline;
+// 		lexing(command_line, &tok);
+		
+		
+// 	}
+// }
+
+// /**
+//  * @brief 
+//  * @param lexing recupere la ligne de commande et creer des char *malloc
+//  * de tokens qui seront mis dans une t_list selon la categorie du token
+//  * @param lexing_spaces si le caractere str[i] est un espace on arrive dans
+//  * cette fonction. @param lexing_spaces malloc et remplit la liste avec un token
+//  * d'espaces.
+//  * @param lexing_quotes malloc + remplit les quotes
+//  * @param is_pipe_bracket malloc + remplit les pipe < > | 
+//  * @param lexing_dollar malloc + remplit les dollars (avec le nom de la variable)
+//  * @param lexing_word malloc + remplit un mot (tout caractere different des precedents)
+//  */
