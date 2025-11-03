ec2ec64091993eb36ae19baf3ba951a233a354f9
Jeanne Delattre <jdelattr@f6r9s15.clusters.42paris.fr>
2025-11-03 15:44:41 +0100

    lundi

--- Header/exec_struct.h
+++ Header/exec_struct.h
@@ -6,7 +6,7 @@
 /*   By: jdelattr <jdelattr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 17:06:42 by jdelattr          #+#    #+#             */
-/*   Updated: 2025/11/02 16:55:28 by jdelattr         ###   ########.fr       */
+/*   Updated: 2025/11/03 15:27:47 by jdelattr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,13 @@ typedef struct s_exec
 	char			**cmds;
 	struct s_redir	*redir;
 	struct s_exec	*next;
+
+	//int			**fd([0][1])in et out pour chaques cmds - 
+	//par default ca sera stdin et stdout.(0 / 1)
+	//int			**pipe; // [in][out] // tab de 2fd
+	//The output of each command in the pipeline is
+	//connected to the input of the next command via a pipe
+
 }	t_exec;
 
 #endif
