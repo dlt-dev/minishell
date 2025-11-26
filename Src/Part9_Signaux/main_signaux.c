bb9fc0c7d2d1f4ec7a184e638324965c26f13cac
Jeanne Delattre <jeanne.delattre45@gmail.com>
2025-11-26 17:07:16 +0100

    fin du tunel

---
+++ Src/Part9_Signaux/main_signaux.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_signaux.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdelattr <jdelattr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/26 15:57:33 by jdelattr          #+#    #+#             */
+/*   Updated: 2025/11/26 16:40:09 by jdelattr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+
+
+/* 	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+
+
+struct sigaction sa;
+
+// 1. Définir le handler
+sa.sa_handler = handle_sigint;
+
+// 2. Initialiser le masque de signaux bloqués (aucun ici)
+sigemptyset(&sa.sa_mask);
+
+// 3. Pas de flags spéciaux (0 par défaut)
+sa.sa_flags = 0;
+
+// Applique cette configuration pour le signal SIGINT (Ctrl+C)
+sigaction(SIGINT, &sa, NULL); */
+
+
+
+void	handle_sigint()
+{
+
+}
