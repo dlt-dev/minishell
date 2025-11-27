2fcd91caef55f4fdf6ecef53ce49a4179553d817
Arthur Oesterle <aoesterl@f1br9s5.clusters.42paris.fr>
2025-11-27 21:04:13 +0100

    a voir signaux

--- Src/main.c
+++ Src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdelattr <jdelattr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aoesterl <aoesterl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 14:04:25 by aoesterl          #+#    #+#             */
-/*   Updated: 2025/11/27 18:50:28 by jdelattr         ###   ########.fr       */
+/*   Updated: 2025/11/27 21:03:18 by aoesterl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,28 @@ volatile sig_atomic_t flag_signal = 0;
 void handle_sigint(int sig)
 {
 	flag_signal = sig + 128;
-	write(1 , "\n", 1);
-	rl_replace_line("", 0);
-	rl_on_new_line(); 
-	rl_redisplay();
 }
-
+static void handle_pending_signal_in_prompt(t_shell *shell)
+{
+    if (flag_signal == 0)
+        return;
+    if (flag_signal == SIGINT)
+    {
+        shell->exit_status = 130;
+    }
+    else if (flag_signal == SIGQUIT)
+    {
+        shell->exit_status = 131;
+    }
+    flag_signal = 0;
+    /* regénérer le prompt avec le nouvel exit_status */
+    if (get_prompt(shell, shell->env, &shell->invite) == ERROR)
+        return;
+    /* réafficher la nouvelle ligne avec le bon prompt */
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
+}
 int	main(int argc, char **argv, char **envp)
 {
 	t_shell	shell;
@@ -65,14 +81,28 @@ int	main(int argc, char **argv, char **envp)
 	if (init_variable(&shell, argc, argv, envp) == ERROR)
 		free_exit(&shell, GEN_ERRNO, NULL);
 	while (1)
-	{
-
+	{	
+		get
+		while(flag_signal != 0)
+		{ 
+			if (get_prompt(&shell, shell.env, &shell.invite) == ERROR)
+			free_exit(&shell, GEN_ERRNO, NULL);
+			shell.rd_line = readline(shell.invite.prompt);
+			
+		}
 		if (get_prompt(&shell, shell.env, &shell.invite) == ERROR)
 			free_exit(&shell, GEN_ERRNO, NULL);
-
 		shell.rd_line = readline(shell.invite.prompt);
-		
-
+        if (flag_signal != 0)
+        {
+            handle_pending_signal_in_prompt(&shell);
+            if (shell.rd_line)
+            {
+                free(shell.rd_line);
+                shell.rd_line = NULL;
+            }
+            continue;
+        }
 		if (shell.rd_line == NULL)
 			free_exit(&shell, GEN_ERRNO, NULL);
 		if (*shell.rd_line != '\0')
@@ -87,7 +117,7 @@ int	main(int argc, char **argv, char **envp)
 		if (delete_quotes(&shell, shell.lst) == ERROR)
 			free_exit(&shell, GEN_ERRNO, NULL);
 		if (logical_struct(&shell, NULL, shell.lst) == ERROR)
-			free_exit(&shell, GEN_ERRNO, NULL);
+			free_exit(&shell, GEN_ERRNO, NULL);c
 		if (shell.cmd_lst)
 			print_cmd_list(shell.cmd_lst);
 		if (check_all_redir(&shell) == ERROR)
