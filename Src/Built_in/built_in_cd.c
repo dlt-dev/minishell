6951aa7525e3fd603bf3c77824aa424f22b2ba65
Arthur Oesterlé <arthur.oesterle@hotmail.com>
2025-12-02 01:51:27 +0100

    over

--- Src/Built_in/built_in_cd.c
+++ Src/Built_in/built_in_cd.c
@@ -6,105 +6,21 @@
 /*   By: aoesterl <aoesterl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 16:56:31 by aoesterl          #+#    #+#             */
-/*   Updated: 2025/12/01 20:09:04 by aoesterl         ###   ########.fr       */
+/*   Updated: 2025/12/02 01:50:54 by aoesterl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*getenv_intern(t_valist *env, char *name)
-{
-	while (env != NULL)
-	{
-		if (strcmp(env->name, name) == 0)
-			return (env->value);
-		env = env->next;
-	}
-	return (NULL);
-}
-
-
-int create_affectation(char *name, char *value)
-{ 
-	t_cb buffer;
-	char *str;
-	
-	init_chunk_buffer(&buffer, 20, 2);
-	if(cb_append_str(&buffer, name) == ERROR)
-		return(ERROR);
-	if(cb_append_char(&buffer, "=") == ERROR)
-		return(ERROR);
-	if(cb_append_str(&buffer, value) == ERROR)
-		return(ERROR);
-	str = fusion_all_chunk(&buffer);
-	if(str == NULL)
-		return(ERROR);
-	return(0);
-}
-
-char ** create_export_tab(char *pwd)
-{ 
-	char **args;
-	int i;
-	
-	i = 0;
-	args = malloc(sizeof(char *) * 3);
-	if(args == NULL)
-		return(NULL);
-	args[0] = ft_strdup("export");
-	if(args[0] == NULL)
-		return(ft_free_tab(args), NULL);
-	args[1] = ft_strdup(pwd);
-	if(args[1] == NULL)
-		return(ft_free_tab(args), NULL);
-	args[2] = NULL;
-	return(0);
-}
-
-int update_env(t_shell *shell, t_valist *env, char *name, char *value)
-{ 
-	char *affectation;
-	char **args;
-	
-	affectation = create_affectation(name, value);
-	if(affectation == NULL)
-		return(GEN_ERRNO);
-	args = create_export_tab(affectation);
-	if(args == NULL)
-		return(free(affectation), GEN_ERRNO);
-	if(builtin_export(shell, env, args) == ERROR)
-		return(free(affectation), ft_free_tab(args), GEN_ERRNO);
-	return(free(affectation), ft_free_tab(args), 0);
-}
-
-int update_cwd(t_shell *shell, t_valist *env, char *name)
-{
-	char *str;
-	char *pwd;
-	char **args; 
-
-	str = getcwd(NULL, 0);
-	if(str == NULL)
-	{ 
-		perror("getcwd");
-		return(GEN_ERRNO);
-	}
-}
-
-int main 
-
-
 
 int	cd_only(t_shell *shell)
 {
 	char	*str;
-	char	*msg;
-
-	msg = "minishell : cd : HOME not set\n";
+	
 	str = getenv_intern(shell->env, "HOME");
 	if (str == NULL)
 	{
-		write(2, msg, ft_strlen(msg));
+		write_str_fd("minishell : cd : HOME not set\n", 2);
 		return (GEN_ERRNO);
 	}
 	else
@@ -115,19 +31,11 @@ int	cd_only(t_shell *shell)
 			return (GEN_ERRNO);
 		}
 	}
-	// export_cwd(shell, shell->env, "PWD");
-	
+	if(update_cwd(shell, shell->env) == ERROR)
+		return(GEN_ERRNO);
 	return (0);
 }
 
-int	too_much_arg(void)
-{
-	char	*msg;
-
-	msg = "minishell: cd : too many arguments\n";
-	write(1, msg, ft_strlen(msg));
-	return (GEN_ERRNO);
-}
 
 int	builtin_cd(t_shell *shell, char **argv)
 {
@@ -139,11 +47,13 @@ int	builtin_cd(t_shell *shell, char **argv)
 	if (i == 1)
 		return (cd_only(shell));
 	if (i > 2)
-		return (too_much_arg());
+		return (write_str_fd("minishell: cd : too many arguments\n", 2), GEN_ERRNO);
 	if (chdir(argv[1]) == ERROR)
 	{
 		print_error_message("cd", argv[1]);
 		return (GEN_ERRNO);
 	}
+	if(update_cwd(shell, shell->env) == ERROR)
+		return(GEN_ERRNO);
 	return (0);
 }
---
+++ Src/Built_in/built_in_cd2.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   built_in_cd2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoesterl <aoesterl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 01:50:04 by aoesterl          #+#    #+#             */
+/*   Updated: 2025/12/02 01:50:45 by aoesterl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*getenv_intern(t_valist *env, char *name)
+{
+	while (env != NULL)
+	{
+		if (strcmp(env->name, name) == 0)
+			return (env->value);
+		env = env->next;
+	}
+	return (NULL);
+}
+
+
+char * create_affectation(char *name, char *value)
+{ 
+	t_cb buffer;
+	char *str;
+	
+	init_chunk_buffer(&buffer, 20, 2);
+	if(cb_append_str(&buffer, name) == ERROR)
+		return(NULL);
+	if(cb_append_char(&buffer, '=') == ERROR)
+		return(NULL);
+	if(cb_append_str(&buffer, value) == ERROR)
+		return(NULL);
+	str = fusion_all_chunk(&buffer);
+	if(str == NULL)
+		return(NULL);
+	return(str);
+}
+
+char ** create_export_tab(char *pwd)
+{ 
+	char **args;
+
+	args = malloc(sizeof(char *) * 3);
+	if(args == NULL)
+		return(NULL);
+	args[0] = ft_strdup("export");
+	if(args[0] == NULL)
+		return(ft_free_tab(args), NULL);
+	args[1] = ft_strdup(pwd);
+	if(args[1] == NULL)
+		return(ft_free_tab(args), NULL);
+	args[2] = NULL;
+	return(args);
+}
+
+int update_env(t_shell *shell, t_valist *env, char *name, char *value)
+{ 
+	char *affectation;
+	char **args;
+	if(name == NULL || value == NULL)
+		return(ERROR);
+	affectation = create_affectation(name, value);
+	if(affectation == NULL)
+		return(GEN_ERRNO);
+	args = create_export_tab(affectation);
+	if(args == NULL)
+		return(free(affectation), GEN_ERRNO);
+	if(builtin_export(shell, env, args) == ERROR)
+		return(free(affectation), ft_free_tab(args), GEN_ERRNO);
+	return(free(affectation), ft_free_tab(args), 0);
+}
+
+int update_cwd(t_shell *shell, t_valist *env)
+{
+	char *pwd; 
+	char *oldpwd;
+
+	pwd = getcwd(NULL, 0);
+	oldpwd = getenv_intern(env, "PWD");
+	if(pwd == NULL)
+		return(perror("getcwd"), ERROR);
+	if(oldpwd != NULL)
+	{
+		if(update_env(shell, env, "OLDPWD", oldpwd) == ERROR)
+			return(free(pwd), ERROR);
+	}
+	if(update_env(shell, env, "PWD", pwd) == ERROR)
+		return(free(pwd), ERROR);
+	free(pwd);
+	return(0);
+}
